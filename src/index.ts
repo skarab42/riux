c27773c7c99201372f05279638a12f44459008ca
skarab42 <skarab@bluewin.ch>
2022-08-07 11:43:51 +0200

    feat: create store

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,51 @@
-export const api = { life: 42 };
+import { type Immutable, produce } from 'immer';
+
+export interface Options {
+  /** Should freeze the initial state outside of `createStore`? (default: true) */
+  freezeInitialState?: boolean;
+}
+
+export const defaultOptions: Required<Options> = {
+  freezeInitialState: true,
+};
+
+export type DefaultOptions = typeof defaultOptions;
+
+export type InitialState<TState, TOptions extends Options> = true extends TOptions['freezeInitialState']
+  ? Immutable<TState>
+  : TState;
+
+export type CurrentState<TState, TOptions extends Options> = Immutable<InitialState<TState, TOptions>>;
+
+export interface Store<TState, TOptions extends Options> {
+  /** Return the initial state (maybe immutable). */
+  initial(): InitialState<TState, TOptions>;
+  /** Return the current state (immutable). */
+  current(): CurrentState<TState, TOptions>;
+}
+
+/**
+ * Create and return a new immutable {@link Store} object.
+ */
+export function createStore<TState, TOptions extends Options = DefaultOptions>(
+  initialState: InitialState<TState, TOptions>,
+  options?: TOptions,
+): Store<TState, TOptions> {
+  const { freezeInitialState } = { ...defaultOptions, ...options };
+
+  if (!freezeInitialState) {
+    initialState = { ...initialState };
+  }
+
+  const currentState = produce(initialState, (draft) => draft) as CurrentState<TState, TOptions>;
+
+  function initial(): InitialState<TState, TOptions> {
+    return initialState;
+  }
+
+  function current(): CurrentState<TState, TOptions> {
+    return currentState;
+  }
+
+  return { initial, current };
+}
--- test/index.test.ts
+++ test/index.test.ts
@@ -1,6 +1,43 @@
-import { expect, test } from 'vitest';
-import { api } from '../src/index.js';
+import { expect, it } from 'vitest';
+import { expectType } from 'vite-plugin-vitest-typescript-assert/tssert';
 
-test('The Ultimate Question of Life', () => {
-  expect(api.life).toBe(42);
+import { createStore } from '../src/index.js';
+
+it('should create an immutable store', () => {
+  const state = { life: 42 };
+  const store = createStore(state);
+
+  expect(store.initial()).toStrictEqual(state);
+  expectType(store.initial()).identicalTo<{ readonly life: number }>();
+
+  expect(store.current()).toStrictEqual(state);
+  expectType(store.current()).identicalTo<{ readonly life: number }>();
+
+  expect(() => {
+    state.life = 24;
+  }).toThrow("Cannot assign to read only property 'life' of object");
+
+  expect(store.initial()).toStrictEqual({ life: 42 });
+  expect(store.current()).toStrictEqual({ life: 42 });
+
+  expect(() => {
+    expectType((store.current().life = 24)).toThrowError(2540);
+  }).toThrow("Cannot assign to read only property 'life' of object");
+});
+
+it('should create an immutable store without freezing the initial store', () => {
+  const state = { life: 42 };
+  const store = createStore(state, { freezeInitialState: false });
+
+  expect(store.initial()).toStrictEqual(state);
+  expectType(store.initial()).identicalTo<{ life: number }>();
+
+  expect(store.current()).toStrictEqual(state);
+  expectType(store.current()).identicalTo<{ readonly life: number }>();
+
+  state.life = 24;
+
+  expect(store.initial()).toStrictEqual({ life: 42 });
+  expect(store.current()).toStrictEqual({ life: 42 });
+  expectType(store.current()).identicalTo<{ readonly life: number }>();
 });
